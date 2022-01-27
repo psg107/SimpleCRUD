68b0fad78bfede5b6f73c81dea842069b7ad83e1
psg107 <psg107@naver.com>
2022-01-27 21:56:01 +0900

    mybatis 초기 세팅 및 로컬 테스트용 커넥션 정보 입력

---
+++ src/main/java/com/sgpark/simplecrud/mapper/ISimpleCRUDMapper.java
@@ -0,0 +1,13 @@
+package com.sgpark.simplecrud.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+@Mapper
+@Repository
+public interface ISimpleCRUDMapper {
+    ArrayList<HashMap<String, Object>> getAllDrink();
+}
