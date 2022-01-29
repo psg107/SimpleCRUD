e0599c9603876487443e92cbf64668d29623f1f2
SGPark <psg107@naver.com>
2022-01-29 13:31:32 +0900

    모델 매핑을 위한 클래스 추가 작업 중

---
+++ src/main/java/com/sgpark/simplecrud/utils/DrinkModelMapper.java
@@ -0,0 +1,17 @@
+package com.sgpark.simplecrud.utils;
+
+import com.sgpark.simplecrud.entity.DrinkEntity;
+import com.sgpark.simplecrud.model.drink.Drink;
+import com.sgpark.simplecrud.utils.base.BaseModelMapper;
+
+public class DrinkModelMapper extends BaseModelMapper<DrinkEntity, Drink> {
+    @Override
+    public DrinkEntity map(Drink item) {
+        return null;
+    }
+
+    @Override
+    public Drink map(DrinkEntity item) {
+        return null;
+    }
+}
