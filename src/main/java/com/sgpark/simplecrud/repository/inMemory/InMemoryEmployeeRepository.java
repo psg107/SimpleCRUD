0c5b39555676f00710a0226a9921acab0fd952b2
SGPark <psg107@naver.com>
2022-01-30 09:29:54 +0900

    리스트 초기화 코드 변경

--- src/main/java/com/sgpark/simplecrud/repository/inMemory/InMemoryEmployeeRepository.java
+++ src/main/java/com/sgpark/simplecrud/repository/inMemory/InMemoryEmployeeRepository.java
@@ -5,16 +5,18 @@ import com.sgpark.simplecrud.repository.inMemory.base.InMemoryRepositoryBase;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
+import java.util.List;
 
 @Component("TestInMemoryEmployeeRepository")
 public class InMemoryEmployeeRepository extends InMemoryRepositoryBase<EmployeeEntity> {
 
     @Override
     public ArrayList<EmployeeEntity> getInMemoryTestList() {
-        var testList = new ArrayList<EmployeeEntity>();
-        testList.add(new EmployeeEntity(0, "박상곤"));
-        testList.add(new EmployeeEntity(1, "김상곤"));
-        testList.add(new EmployeeEntity(2, "이상곤"));
+        var testList = new ArrayList<EmployeeEntity>(List.of(
+            new EmployeeEntity(0, "박상곤"),
+            new EmployeeEntity(1, "김상곤"),
+            new EmployeeEntity(2, "이상곤")
+        ));
 
         return testList;
     }
