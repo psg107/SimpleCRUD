b6e8e1d72c249b10d722c5123d89543b135ced79
SGPark <psg107@naver.com>
2022-01-30 10:29:01 +0900

    Revert "모델 매핑을 위한 클래스 추가 작업 중"
    This reverts commit e0599c9603876487443e92cbf64668d29623f1f2.

--- src/main/java/com/sgpark/simplecrud/utils/base/BaseModelMapper.java
+++
@@ -1,9 +0,0 @@
-package com.sgpark.simplecrud.utils.base;
-
-import com.sgpark.simplecrud.entity.base.BaseEntity;
-
-public abstract class BaseModelMapper<T1 extends BaseEntity, T2> {
-    public abstract T1 map(T2 item);
-
-    public abstract T2 map(T1 item);
-}
